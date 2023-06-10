e1e110d7c68f191b88cebe885102349c584af2c3
unknown <Febbie@Febbie.com>
2023-06-10 22:12:37 +0100

    First commit

---
+++ src/com/company/AccountHolder.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class AccountHolder {
+    String  firstName;
+    String lastName;
+    int age;
+    float accountBalance;
+    boolean eligibleForCreditCard;
+
+    public void testEligibilityForCreditCards () {
+        if (age >25 && accountBalance>=20000) {
+            eligibleForCreditCard=true;
+        }
+    }
+}
